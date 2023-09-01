ab2714d624da94b87d3e41cd0ea96fc9a8d489ac
Moustapha Kebe <moustaphakebe@Moustaphas-MacBook-Air.local>
2023-09-01 13:14:56 +0000

    first commit

---
+++ NekiBreakfast/Controllers/BreakfastsController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using NekiBreakfast.Contracts.Breakfast;
+
+namespace NekiBreakfast.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+// [Route("breakfasts")] same as above
+public class BreakfastsController : ControllerBase
+{
+    [HttpPost()]
+    public IActionResult CreateBreakfast(CreateBreakfastRequest request) {
+        return Ok(request);
+    }
+
+    [HttpGet("{id:guid}")]
+    public IActionResult GetBreakfast(Guid id) {
+        return Ok(id);
+    }
+
+    [HttpPut("{id:guid}")]
+    public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request) {
+        return Ok(request);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public IActionResult DeleteBreakfast(Guid id) {
+        return Ok(id);
+    }
+}
